af71875554086320bc2a819dbaa1c9ae250f2903
wesleyroseno <37702941+wesleyroseno@users.noreply.github.com>
2018-08-07 20:11:11 -0300

    Aperfeiçoamento do main de anotacao.

--- src/classes/DiarioDeBordoOO/MainAnotacao.java
+++ src/classes/DiarioDeBordoOO/MainAnotacao.java
@@ -9,9 +9,12 @@ public class MainAnotacao {
 		Anotacao a1 = new Anotacao("criacao de classes", "20/04/2018");
 		String texto = sc.nextLine();
 		String data = sc.nextLine();
+		sc.close();
 		Anotacao a2 = new Anotacao(texto, data);
 		a2.setDesempenho(500);
 		System.out.println(a2.getDesempenho());
+		System.out.println(a1);
+		System.out.println(a2);
 	
 	}
 
